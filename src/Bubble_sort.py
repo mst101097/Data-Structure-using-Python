510b2a2e1853d61b644839709e8fe40b01b11f23
mst101097 <singh.1097.mohit@gmail.com>
2020-09-13 17:05:27 +0530

    @sorting using python

---
+++ src/Bubble_sort.py
@@ -0,0 +1,14 @@
+#Bubble sort
+def Bubble(arr):
+    n= len(arr)
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                print("swaping" + str(arr[j]) + "and" + str(arr[j+1]))
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                print(arr)
+
+arr= [9,5,2,6]
+Bubble(arr)
+print(arr)
+
